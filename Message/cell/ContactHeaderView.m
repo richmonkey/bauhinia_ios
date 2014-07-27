732c08b059377b4d6b5c4b9625998e6d89c000e6
daibou007 <daibou007@gmail.com>
2014-07-27 22:43:18 +0800

    补充提交文件；

---
+++ Message/cell/ContactHeaderView.m
@@ -0,0 +1,28 @@
+//
+//  ContactHeaderView.m
+//  Message
+//
+//  Created by daozhu on 14-7-27.
+//  Copyright (c) 2014年 daozhu. All rights reserved.
+//
+
+#import "ContactHeaderView.h"
+
+@implementation ContactHeaderView
+
+
+
+-(void)awakeFromNib{
+
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
