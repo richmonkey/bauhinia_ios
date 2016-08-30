34b25797525f713c08601034a2ac67f49d28c76a
houxh <houxuehua49@gmail.com>
2016-08-30 14:46:16 +0800

    build amr lib flag:-fembed-bitcode-marker

--- ios/Message/backend/amr/build.sh
+++ ios/Message/backend/amr/build.sh
@@ -31,7 +31,7 @@ do
         fi
         PATH=`xcodebuild -version -sdk iphoneos PlatformPath`"/Developer/usr/bin:$PATH" \
         SDK=`xcodebuild -version -sdk iphoneos Path` \
-        CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include -fembed-bitcode" \
+        CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include -fembed-bitcode-marker" \
         LDFLAGS="-Wl,-syslibroot,$SDK" \
         ./configure \
         --host=arm-apple-darwin \
@@ -41,7 +41,7 @@ do
     *)
         echo "Building opencore-amr for iPhoneSimulator $arch *****************"
         PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
-        CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -fembed-bitcode" \
+        CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -fembed-bitcode-marker" \
         ./configure \
         --prefix=$DEST/$arch \
         --disable-shared
