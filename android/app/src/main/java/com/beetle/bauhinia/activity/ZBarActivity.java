5b1d3e262192c2c74d0e2b78ab62524a41afd8ba
houxh <houxuehua49@gmail.com>
2017-10-12 19:37:38 +0800

    bugfix:升级zbar

--- android/app/src/main/java/com/beetle/bauhinia/activity/ZBarActivity.java
+++ android/app/src/main/java/com/beetle/bauhinia/activity/ZBarActivity.java
@@ -12,31 +12,18 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.FrameLayout;
-import android.widget.Button;
-
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.AutoFocusCallback;
-import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
-
-import android.widget.TextView;
-import android.graphics.ImageFormat;
-
 import com.beetle.bauhinia.R;
+import com.yanzhenjie.zbar.Config;
+import com.yanzhenjie.zbar.Image;
+import com.yanzhenjie.zbar.ImageScanner;
+import com.yanzhenjie.zbar.Symbol;
+import com.yanzhenjie.zbar.SymbolSet;
 
-/* Import ZBar Class files */
-import net.sourceforge.zbar.ImageScanner;
-import net.sourceforge.zbar.Image;
-import net.sourceforge.zbar.Symbol;
-import net.sourceforge.zbar.SymbolSet;
-import net.sourceforge.zbar.Config;
 
 public class ZBarActivity extends Activity
 {
